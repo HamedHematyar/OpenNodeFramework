b897b17628ee823f544dfdf68ff9274be7492521
hamed.hematyar <hamed.yar@outlook.com>
2024-05-15 15:52:18 -0700

    updated registry.py

--- core/registry.py
+++ core/registry.py
@@ -4,5 +4,5 @@ from core.concrete import attribute
 Nodes = {cls.__name__: cls for cls in [node.NullNode,
                                        ]}
 
-Attributes = {cls.__name__: cls for cls in [attribute.NameAttribute,
+Attributes = {cls.__name__: cls for cls in [attribute.StringAttribute,
                                             ]}
