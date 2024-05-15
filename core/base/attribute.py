c10e5144286cf2eca11191116204e2148d6d0144
hamed.hematyar <hamed.yar@outlook.com>
2024-05-15 13:52:15 -0700

    using MutableMapping instead of multi inheritance

--- core/base/attribute.py
+++ core/base/attribute.py
@@ -1,29 +1,24 @@
-from collections.abc import MutableMapping, MutableSequence
-
-from core.abstract import attribute
+from collections.abc import MutableMapping
 
 
-def integrate_validator(func):
+def dict_item_validator_wrapper(func):
     """
     :param func: the function to be wrapped
     :return: the wrapped function
 
-    This method takes a function as a parameter and returns a wrapped function. The wrapped function executes the
-    given function, but before doing so, it calls a validator method corresponding to the provided key. If the
-    validator method exists, it is called with the key and value parameters. If the validator method does not exist,
-    a default lambda function is called instead. Finally, the given function is executed with the key and value
-    parameters.
+    This method calls a validator method corresponding to the provided key.
 
     """
-
     def wrapper(self, key, value):
-        getattr(self, f"{key}_validator", lambda *args: None)(key, value)
+        validation_method = getattr(self, f"{key}_validator", lambda *args: None)
+        validation_method(key, value)
+
         func(self, key, value)
 
     return wrapper
 
 
-class Attribute(attribute.AbstractAttribute, MutableMapping):
+class Attribute(MutableMapping):
     """
     A concrete attribute class that implements AbstractAttribute and MutableMapping.
     """
@@ -34,7 +29,7 @@ class Attribute(attribute.AbstractAttribute, MutableMapping):
         for key, value in kwargs.items():
             self.__setitem__(key, value)
 
-    @integrate_validator
+    @dict_item_validator_wrapper
     def __setitem__(self, key, value):
         self._data[key] = value
 
@@ -59,33 +54,30 @@ class Attribute(attribute.AbstractAttribute, MutableMapping):
             raise TypeError(f'attribute {key} must be a string.')
 
 
-class AttributeCollection(attribute.AbstractAttributeCollection, MutableSequence):
-    def __init__(self, attributes: list):
-        self._data = list(*attributes)
+class AttributeCollection(MutableMapping):
+    def __init__(self, **kwargs):
+        self._data = {}
 
-    def __getitem__(self, index):
-        return self._data[index]
+        for key, value in kwargs.items():
+            self.__setitem__(key, value)
 
-    def __setitem__(self, index, value):
-        self._data[index] = value
+    def __setitem__(self, key, value):
+        if not isinstance(value, Attribute):
+            raise TypeError(f'attribute {value} is not an instance of {Attribute}')
 
-    def __delitem__(self, index):
-        del self._data[index]
+        self._data[key] = value
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
 
     def __len__(self):
         return len(self._data)
 
     def __repr__(self):
         return repr(self._data)
-
-    def insert(self, index, value):
-        if not isinstance(value, attribute.AbstractAttribute):
-            raise TypeError(f'attribute {value} is not an instance of {attribute.AbstractAttribute}')
-
-        self._data.insert(index, value)
-
-    def append(self, value):
-        if not isinstance(value, attribute.AbstractAttribute):
-            raise TypeError(f'attribute {value} is not an instance of {attribute.AbstractAttribute}')
-
-        self._data.append(value)
