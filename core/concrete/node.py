b75e8777eed067cdfabf17013b7bd49a6ab2945c
hamed.hematyar <hamed.yar@outlook.com>
2024-05-14 17:22:43 -0700

    concrete node draft

--- core/concrete/node.py
+++ core/concrete/node.py
@@ -1,7 +1,11 @@
 from core.base import node
+from core.concrete import attribute
 
 
 class NullNode(node.Node):
     def __init__(self):
         super().__init__()
+
+    def initialize_attributes(self):
+        self.attributes.append(attribute.NameAttribute())
 
