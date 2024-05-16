3ea63fb6ad9c4d5f066d5c5ae727702dbaf54c0c
hamed.hematyar <hamed.yar@outlook.com>
2024-05-16 11:29:57 -0700

    re implementing connection.py

--- core/abstract/connection.py
+++ core/abstract/connection.py
@@ -14,12 +14,9 @@ class AbstractConnection(ABC):
         """This should return the source attribute of the connection."""
 
     @abstractmethod
-    def get_valid_source_types(self) -> typing.List[Attribute]:
-        """This should return a list of valid source attribute types."""
-
-    @abstractmethod
     def get_destination(self) -> Attribute:
         """This should return the destination attribute of the connection."""
 
-    def get_valid_destination_types(self) -> typing.List[Attribute]:
-        """This should return a list of valid destination attribute types."""
+    @abstractmethod
+    def get_data(self) -> typing.Any:
+        """This should give access to connection data"""
