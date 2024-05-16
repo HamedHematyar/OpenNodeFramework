1ddabe6b9811f33cf4a884ac17a4e4f686e503cd
hamed.hematyar <hamed.yar@outlook.com>
2024-05-15 17:27:09 -0700

    wire drafts

---
+++ core/abstract/wire.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import typing
+
+from core.base.attribute import *
+
+
+class AbstractWire(ABC):
+    """
+    An abstract class representing a plug.
+    """
+
+    @abstractmethod
+    def get_source(self) -> Attribute:
+        """This should return the source node of the plug."""
+
+    @abstractmethod
+    def get_valid_source_types(self) -> typing.List [typing.Any]:
+        """This should return a list of valid source attribute types."""
+
+    @abstractmethod
+    def get_destination(self) -> Attribute:
+        """This should return the destination node of the plug."""
+
+    def get_valid_destination_types(self) -> typing.List[typing.Any]:
+        """This should return a list of valid destination attribute types."""
