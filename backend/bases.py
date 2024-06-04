0b3924a99a1b2249299a686335d5706c726c3c54
hamed.hematyar <hamed.yar@outlook.com>
2024-06-04 09:17:39 -0700

    isolated all the base classes in bases.py

---
+++ backend/bases.py
@@ -0,0 +1,404 @@
+import typing as t
+from collections.abc import MutableMapping, MutableSequence
+
+from backend.abstracts import (AbstractAttribute,
+                               AbstractNode,
+                               AbstractPort,
+                               PortType,
+                               AbstractGraph)
+
+
+class TypedList(MutableSequence):
+    def __init__(self, types: tuple):
+        super().__init__()
+
+        self.data = list()
+        self.types = types
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    def __setitem__(self, idx, value):
+        if not isinstance(value, self.types):
+            raise TypeError(f'value must be of type {self.types}')
+
+        self.data[idx] = value
+
+    def __delitem__(self, idx):
+        del self.data[idx]
+
+    def insert(self, idx, value):
+        if not isinstance(value, self.types):
+            raise TypeError(f'value must be of type {self.types}')
+
+        self.data.insert(idx, value)
+
+    def __repr__(self):
+        return str(self.data)
+
+
+class BaseAttribute(AbstractAttribute):
+    """
+    A concrete attribute class that implements AbstractAttribute.
+    """
+
+    def __init__(self, name, value):
+        self.__name: t.Optional[str] = None
+        self.__value: t.Optional[t.Any] = None
+        self.__node: t.Optional[BaseNode] = None
+
+        self.name = name
+        self.value = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f'attribute name must be a string.')
+
+        self.__name = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    @property
+    def node(self) -> 'BaseNode':
+        return self.__node
+
+    @node.setter
+    def node(self, value: 'BaseNode'):
+        if not isinstance(value, BaseNode):
+            raise TypeError(f'node {value} is not an instance of {BaseNode}')
+
+        self.__node = value
+
+
+class BaseAttributeCollection(MutableMapping):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._data = {}
+        self.__node: t.Optional[BaseNode] = None
+
+        self.update(**kwargs)
+
+    def __setitem__(self, key: str, value: BaseAttribute):
+        if not isinstance(value, BaseAttribute):
+            raise TypeError(f'attribute {value} is not an instance of {BaseAttribute}')
+
+        if self.node:
+            value.node = self.node
+
+        self._data[key] = value
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __repr__(self):
+        return repr(self._data)
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__setitem__(key, value)
+
+    def add(self, attribute: BaseAttribute):
+        self.update(**{attribute.name: attribute})
+
+    @property
+    def node(self) -> 'BaseNode':
+        return self.__node
+
+    @node.setter
+    def node(self, value: 'BaseNode'):
+        if not isinstance(value, BaseNode):
+            raise TypeError(f'graph {value} is not an instance of {BaseNode}')
+
+        self.__node = value
+
+
+class BasePort(AbstractPort):
+    def __init__(self, name: str, mode: PortType):
+        self.__name: t.Optional[str] = None
+        self.__mode: t.Optional[PortType] = None
+        self.__node: t.Optional[BaseNode] = None
+
+        self.name = name
+        self.mode = mode
+
+        self.connections: BasePortCollection[BasePort] = BasePortCollection()
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if not isinstance(name, str):
+            raise TypeError(f'port {name} is not an instance of {str}')
+
+        self.__name = name
+
+    @property
+    def mode(self) -> PortType | None:
+        return self.__mode
+
+    @mode.setter
+    def mode(self, mode: PortType):
+        if not isinstance(mode, PortType):
+            raise TypeError(f'port type {mode} is not an instance of {PortType}')
+
+        self.__mode = mode
+
+    @property
+    def node(self) -> t.Optional['BaseNode']:
+        return self.__node
+
+    @node.setter
+    def node(self, node: 'BaseNode'):
+        if not isinstance(node, BaseNode):
+            raise TypeError(f'node {node} is not an instance of {BaseNode}')
+
+        self.__node = node
+
+
+class BasePortCollection(TypedList):
+    def __init__(self):
+        super().__init__(self.__class__.__mro__)
+
+        self.__node: t.Optional[BaseNode] = None
+
+    def __setitem__(self, idx, value):
+        super().__setitem__(idx, value)
+
+        if self.node:
+            value.node = self.node
+
+    def insert(self, idx, value):
+        super().insert(idx, value)
+
+        if self.node:
+            value.node = self.node
+
+    def disconnect(self, index: int):
+        self.pop(index)
+
+    def has_connections(self):
+        return bool(len(self))
+
+    @property
+    def node(self) -> 'BaseNode':
+        return self.__node
+
+    @node.setter
+    def node(self, value: 'BaseNode'):
+        if not isinstance(value, BaseNode):
+            raise TypeError(f'graph {value} is not an instance of {BaseNode}')
+
+        self.__node = value
+
+
+class BaseNode(AbstractNode):
+    def __init__(self):
+        """
+        Implement the base class of AbstractNode.
+        """
+        self.__graph: t.Optional[BaseGraph] = None
+
+        self.attributes = None
+        self.inputs = None
+        self.outputs = None
+
+    def compute_data(self) -> t.Optional[t.Any]:
+        return
+
+    def compute_output(self, output_port: AbstractPort) -> t.Optional[t.Any]:
+        return getattr(self, f'_compute_{output_port.name}_port')()
+
+    @property
+    def graph(self) -> t.Optional['BaseGraph']:
+        return self.__graph
+
+    @graph.setter
+    def graph(self, value: 'BaseGraph'):
+        if not isinstance(value, BaseGraph):
+            raise TypeError(f'graph {value} is not an instance of {BaseGraph}')
+
+        self.__graph = value
+
+    @property
+    def attributes(self) -> t.Optional['BaseAttributeCollection']:
+        return self.__attributes
+
+    @attributes.setter
+    def attributes(self, value: 'BaseAttributeCollection'):
+        if not isinstance(value, BaseAttributeCollection):
+            raise TypeError(f'{value} is not an instance of {BaseAttributeCollection}')
+
+        value.node = self
+        self.__attributes = value
+
+    @property
+    def inputs(self) -> t.Optional['BasePortCollection']:
+        return self.__inputs
+
+    @inputs.setter
+    def inputs(self, value: 'BasePortCollection'):
+        if not isinstance(value, BasePortCollection):
+            raise TypeError(f'{value} is not an instance of {BasePortCollection}')
+
+        value.node = self
+        self.__inputs = value
+
+    @property
+    def outputs(self) -> t.Optional['BasePortCollection']:
+        return self.__outputs
+
+    @outputs.setter
+    def outputs(self, value: 'BasePortCollection'):
+        if not isinstance(value, BasePortCollection):
+            raise TypeError(f'{value} is not an instance of {BasePortCollection}')
+
+        value.node = self
+        self.__outputs = value
+
+
+class BaseNodeCollection(TypedList):
+    def __init__(self):
+        super().__init__(self.__class__.__mro__)
+
+        self.__graph: t.Optional[BaseGraph] = None
+
+    def __setitem__(self, idx, value):
+        super().__setitem__(idx, value)
+
+        if self.graph:
+            value.graph = self.graph
+
+    def insert(self, idx, value):
+        super().insert(idx, value)
+
+        if self.graph:
+            value.graph = self.graph
+
+    def has_nodes(self):
+        return bool(len(self))
+
+    @property
+    def graph(self) -> t.Optional['BaseGraph']:
+        return self.__graph
+
+    @graph.setter
+    def graph(self, value: 'BaseGraph'):
+        if not isinstance(value, BaseGraph):
+            raise TypeError(f'graph {value} is not an instance of {BaseGraph}')
+
+        self.__graph = value
+
+
+class BaseGraph(AbstractGraph):
+    def __init__(self, name: str):
+        self.__name: t.Optional[str] = None
+        self.__parent = None
+        self.__nodes = None
+        self.__graphs = None
+
+        self.name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: t.Optional[str]):
+        if not isinstance(value, str):
+            raise TypeError(f'{value} is not an instance of {str}')
+
+        self.__name = value
+
+    @property
+    def parent(self):
+        return self.__name
+
+    @parent.setter
+    def parent(self, value: t.Optional[str]):
+        if not isinstance(value, BaseGraph):
+            raise TypeError(f'{value} is not an instance of {BaseGraph}')
+
+        self.__parent = value
+
+    @property
+    def nodes(self):
+        return self.__nodes
+
+    @nodes.setter
+    def nodes(self, value):
+        if not isinstance(value, BaseNodeCollection):
+            raise TypeError(f'{value} is not an instance of {BaseNodeCollection}')
+
+        value.graph = self
+        self.__nodes = value
+
+    @property
+    def graphs(self):
+        return self.__graphs
+
+    @graphs.setter
+    def graphs(self, value):
+        if not isinstance(value, BaseGraphCollection):
+            raise TypeError(f'{value} is not an instance of {list}')
+
+        value.parent = self
+        self.__graphs = value
+
+
+class BaseGraphCollection(TypedList):
+    def __init__(self):
+        super().__init__(self.__class__.__mro__)
+
+        self.__parent: t.Optional[BaseGraph] = None
+
+    def __setitem__(self, idx, value):
+        super().__setitem__(idx, value)
+
+        if self.parent:
+            value.parent = self.parent
+
+    def insert(self, idx, value):
+        super().insert(idx, value)
+
+        if self.parent:
+            value.parent = self.parent
+
+    def has_nodes(self):
+        return bool(len(self))
+
+    @property
+    def parent(self) -> t.Optional['BaseGraph']:
+        return self.__parent
+
+    @parent.setter
+    def parent(self, value: 'BaseGraph'):
+        if not isinstance(value, BaseGraph):
+            raise TypeError(f'graph {value} is not an instance of {BaseGraph}')
+
+        self.__parent = value
