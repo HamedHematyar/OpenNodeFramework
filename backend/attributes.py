c07c91c0d090062b019326bce4c122fc922d4f0a
hamed.hematyar <hamed.yar@outlook.com>
2024-06-04 09:04:30 -0700

    move concrete attributes to attribute.py

---
+++ backend/attributes.py
@@ -0,0 +1,80 @@
+from backend.bases import BaseAttribute
+
+
+class String(BaseAttribute):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f'attribute value must be a str.')
+
+        self.__value = value
+
+
+class Integer(BaseAttribute):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f'attribute value must be a int.')
+
+        self.__value = value
+
+
+class List(BaseAttribute):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, list):
+            raise TypeError(f'attribute value must be a list.')
+
+        self.__value = value
+
+
+class TypeAttribute(BaseAttribute):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            value = eval(value)
+
+        if value not in [str,
+                         int,
+                         list]:
+            raise TypeError(f'attribute value is not valid.')
+
+        self.__value = value.__name__
+
+
+class NameAttribute(String):
+    """
+    >>> NameAttribute(value='test')
+    {'name': 'name', 'value': 'test'}
+    """
+
+    def __init__(self, value):
+        super().__init__('name', value)
