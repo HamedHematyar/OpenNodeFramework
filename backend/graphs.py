1ea781f0cc7469299681dc596598339478151309
hamed.hematyar <hamed.yar@outlook.com>
2024-06-04 09:07:59 -0700

    moved concrete graphs to graphs.py

---
+++ backend/graphs.py
@@ -0,0 +1,11 @@
+from backend.bases import BaseGraph
+from backend.nodes import Node
+from backend.collections import NodeCollection, GraphCollection
+
+
+class Graph(BaseGraph):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+        self.nodes: NodeCollection[Node] = NodeCollection()
+        self.graphs: GraphCollection[Graph] = GraphCollection()
