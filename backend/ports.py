fb2c775351ae0d8adfa7e1d773df6abf8e7f6f4e
hamed.hematyar <hamed.yar@outlook.com>
2024-06-10 17:53:41 -0700

    update concrete ports

--- backend/ports.py
+++ backend/ports.py
@@ -2,10 +2,10 @@ from backend.bases import BasePort
 
 
 class InPort(BasePort):
-    def __init__(self, name, mode):
-        super().__init__(name, mode)
+    def __init__(self):
+        super().__init__()
 
 
 class OutPort(BasePort):
-    def __init__(self, name, mode):
-        super().__init__(name, mode)
+    def __init__(self):
+        super().__init__()
