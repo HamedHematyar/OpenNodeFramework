ed19105939b6dfc0dfbcfe754f9bd0c8b4654cb1
hamed.hematyar <hamed.yar@outlook.com>
2024-06-12 13:56:10 -0700

    draft implementation of data method for input and output ports

--- backend/ports.py
+++ backend/ports.py
@@ -5,8 +5,14 @@ class InputPort(BasePort):
     def __init__(self):
         super().__init__()
 
+    def data(self, connection_index=0):
+        return self.connections[connection_index].data()
 
 
 class OutputPort(BasePort):
     def __init__(self):
         super().__init__()
+
+    def data(self, connection_index=0):
+        return self.node.data()
+    
