8afb88409428ebe3b803dc9ac6e3ffc21eeae3ee
hamed.hematyar <hamed.yar@outlook.com>
2024-06-12 17:05:29 -0700

    updating draft concrete attributes

--- backend/attributes.py
+++ backend/attributes.py
@@ -1,3 +1,4 @@
+import typing as t
 from backend.bases import BaseAttribute
 
 
@@ -5,60 +6,67 @@ class String(BaseAttribute):
     def __init__(self):
         super().__init__()
 
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
+    def set_value(self, value):
         if not isinstance(value, str):
             raise TypeError(f'attribute value must be a str.')
 
-        self.__value = value
+        super().set_value(value)
 
 
 class Integer(BaseAttribute):
     def __init__(self):
         super().__init__()
 
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
+    def set_value(self, value):
         if not isinstance(value, (int, float)):
             raise TypeError(f'attribute value must be a int.')
 
-        self.__value = value
+        super().set_value(value)
 
 
 class List(BaseAttribute):
     def __init__(self):
         super().__init__()
 
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
+    def set_value(self, value):
         if not isinstance(value, list):
             raise TypeError(f'attribute value must be a list.')
 
-        self.__value = value
+        super().set_value(value)
+
+
+class Enumeration(BaseAttribute):
+    def __init__(self):
+        super().__init__()
+
+        self._options: t.Optional[t.List[t.Any]] = None
+
+    def initialize(self, name, options):
+        super().initialize(name, options)
+
+        self.set_options(options)
+        self.set_value(options[0])
+
+        return self
+
+    def get_options(self):
+        return self._options
+
+    def set_options(self, options):
+        if not isinstance(options, list):
+            raise TypeError(f'options is not an instance of {list}.')
+
+        if options:
+            raise ValueError(f'no valid options are given : {options}')
+
+        self._options = options
 
 
 class TypeAttribute(BaseAttribute):
     def __init__(self):
         super().__init__()
 
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
+    def set_value(self, value):
         if isinstance(value, str):
             value = eval(value)
 
@@ -67,9 +75,4 @@ class TypeAttribute(BaseAttribute):
                          list]:
             raise TypeError(f'attribute value is not valid.')
 
-        self.__value = value.__name__
-
-
-class NameAttribute(String):
-    def __init__(self):
-        super().__init__()
+        super().set_value(value.__name__)
