b88b8ac2bdaa448693d7ddd835051c991ffd6f81
hamed.hematyar <hamed.yar@outlook.com>
2024-06-14 15:32:57 -0700

    cleanup

--- custom/attributes/attributes.py
+++ custom/attributes/attributes.py
@@ -1,9 +1,9 @@
-from backend.attributes import String
+from backend.attributes import GenericStr
 from backend.registry import register_custom_attribute
 
 
 @register_custom_attribute
-class NodeName(String):
+class NodeName(GenericStr):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
