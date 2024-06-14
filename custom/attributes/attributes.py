887ba7d9a5d25a5c4340a7d9a8cae98fd75e37d7
hamed.hematyar <hamed.yar@outlook.com>
2024-06-14 12:12:14 -0700

    sample custom attribute

--- custom/attributes/attributes.py
+++ custom/attributes/attributes.py
@@ -4,8 +4,6 @@ from backend.registry import register_custom_attribute
 
 @register_custom_attribute
 class NodeName(String):
-    valid_types = (str, )
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
