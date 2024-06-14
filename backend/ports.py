3db0ab5f52c59b478367ebb3e5ec1198535d5df1
hamed.hematyar <hamed.yar@outlook.com>
2024-06-14 16:37:02 -0700

    update port subclasses init signature

--- backend/ports.py
+++ backend/ports.py
@@ -2,17 +2,17 @@ from backend.bases import BasePort
 
 
 class InputPort(BasePort):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def data(self, connection_index=0):
         return self.connections[connection_index].data()
 
 
 class OutputPort(BasePort):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def data(self, connection_index=0):
-        return self.node.data()
+        return self.get_parent().data()
     
