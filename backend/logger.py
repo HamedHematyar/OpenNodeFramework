df381754fd82fc6b61aa7e974d18ca2feac17c2e
hamed.hematyar <hamed.yar@outlook.com>
2024-06-14 10:23:05 -0700

    more info on the logs

--- backend/logger.py
+++ backend/logger.py
@@ -1,4 +1,12 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(funcName)s - %(lineno)d - %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
 logger = logging.getLogger(__name__)
