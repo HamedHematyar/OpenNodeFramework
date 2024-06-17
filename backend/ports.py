14f95fd0048174d823d6287c76fc318636dacacd
hamed.hematyar <hamed.yar@outlook.com>
2024-06-17 16:25:04 -0700

    update how we instance port collections

--- backend/ports.py
+++ backend/ports.py
@@ -1,18 +1,22 @@
 from backend.bases import BasePort
+from backend.aggregations import PortCollection
 
 
 class InputPort(BasePort):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.set_connections(PortCollection())
+
     def data(self, connection_index=0):
-        return self.connections[connection_index].data()
+        return self.get_connections()[connection_index].data()
 
 
 class OutputPort(BasePort):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.set_connections(PortCollection())
+
     def data(self, connection_index=0):
         return self.get_parent().data()
-    
