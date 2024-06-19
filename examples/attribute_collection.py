86af1e5f9bf7efeb9f9603c5c773d87f34ffa47c
hamed.hematyar <hamed.yar@outlook.com>
2024-06-18 17:00:41 -0700

    draft examples for AttributeCollection

--- examples/attribute_collection.py
+++ examples/attribute_collection.py
@@ -1,17 +1,41 @@
-from backend.attributes import GenericInt, GenericStr
+from backend.attributes import GenericStr, GenericInt
 from backend.aggregations import AttributeCollection
 
-attribute_collection = AttributeCollection()
 
-int_constant_01 = GenericInt(name='int_constant', value=10)
-str_constant_02 = GenericStr(name='str_constant', value='test')
+if __name__ == '__main__':
+    attr_01 = GenericStr(name='name', value='test_name')
+    print(attr_01.dumps(indent=4))
 
-attribute_collection.append(int_constant_01)
-attribute_collection.append(str_constant_02)
+    serialized_attribute = attr_01.serialize()
+    print(serialized_attribute)
 
-serialized = attribute_collection.serialize()
-print(serialized)
+    deserialized_attribute = GenericStr.deserialize(serialized_attribute, relations=True)
+    print(deserialized_attribute.dumps(indent=4))
 
+    path = "../dump/attribute.json"
+    dumped_attribute = attr_01.dump(path, indent=4)
 
-deserialized = attribute_collection.deserialize(serialized, relations=True)
-print(deserialized)
+    loaded_attribute = GenericStr.load(path)
+    print(loaded_attribute.dumps(indent=4))
+
+    attr_02 = GenericStr(name='label', value="name")
+    attr_03 = GenericInt(name='size', value=5)
+
+    attr_collection = AttributeCollection()
+    attr_collection.append(attr_01)
+    attr_collection.append(attr_02)
+    attr_collection.append(attr_03)
+
+    print(attr_collection.dumps(indent=4))
+
+    serialized_collection = attr_collection.serialize()
+    print(serialized_collection)
+
+    deserialized_collection = AttributeCollection.deserialize(serialized_collection, relations=True)
+    print(deserialized_collection.dumps(indent=4))
+
+    path = "../dump/attribute_collection.json"
+    dumped_collection = attr_collection.dump(path, indent=4)
+
+    loaded_collection = AttributeCollection.load(path)
+    print(loaded_collection.dumps(indent=4))
