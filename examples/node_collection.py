ea64eb41eb0696ca11491b3a2e8e06fb7e06744c
hamed.hematyar <hamed.yar@outlook.com>
2024-06-21 16:15:47 -0700

    cleanup

--- examples/node_collection.py
+++
@@ -1,9 +0,0 @@
-from backend.nodes import ParameterNode, SumNode, Node
-from backend.aggregations import NodeCollection
-
-node_collection = NodeCollection()
-print(node_collection)
-
-param_node = ParameterNode(value=5)
-node_collection.append(param_node)
-print(node_collection)
