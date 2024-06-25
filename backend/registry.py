f985aa599ba6ae6bd89cbeea0d6867aae34454ef
hamed.hematyar <hamed.yar@outlook.com>
2024-06-24 17:10:38 -0700

    new register method

--- backend/registry.py
+++ backend/registry.py
@@ -1,68 +1,51 @@
-import threading
+import typing as t
+from backend.logger import logger
 
-from backend.data_types import *
-from backend.attributes import *
-from backend.nodes import *
-from backend.aggregations import *
-from backend.events import *
-from backend.validators import *
-from backend.ports import *
-
-
-Lock = threading.Lock()
+RegisteredNodes = {}
+RegisteredTypes: t.Dict[str, type] = {}
+RegisteredAttributes: t.Dict[str, type] = {}
+RegisteredPorts = {}
+RegisteredCollections = {}
+RegisteredEvents = {}
 
 
-RegisteredNodes = {entry.__name__: entry for entry in [Node,
-                                                       SumNode,
-                                                       ParameterNode
-                                                       ]}
-
-RegisteredTypes: t.Dict[str, type] = {entry.__name__: entry for entry in [GenericStr,
-                                                                          GenericInt,
-                                                                          GenericFloat,
-                                                                          GenericList,
-                                                                          GenericEnum,
-                                                                          DataTypeEnum,
-                                                                          PortModeEnum,
-                                                                          ReferencedNodeAttribute,
-                                                                          ReferencedNode
-                                                                          ]}
-
-RegisteredAttributes: t.Dict[str, type] = {entry.__name__: entry for entry in [StringAttribute,
-                                                                               ]}
-
-RegisteredPorts = {entry.__name__: entry for entry in [GenericPort,
-                                                       ]}
-
-RegisteredCollections = {entry.__name__: entry for entry in [DataTypeCollection,
-                                                             AttributeTypesCollection,
-                                                             AttributeCollection,
-                                                             PortCollection,
-                                                             PortAttributesCollection,
-                                                             NodeCollection
-                                                             ]}
-
-RegisteredEvents = {entry.__name__: entry for entry in [PreNodeInitialized,
-                                                        PostNodeInitialized,
-                                                        PreNodeDeleted,
-                                                        PostNodeDeleted,
-
-                                                        PreTypeInitialized,
-                                                        PostTypeInitialized,
-                                                        PreTypeDeleted,
-                                                        PostTypeDeleted,
-                                                        PreTypeDataChanged,
-                                                        PostTypeDataChanged,
-
-                                                        PrePortInitialized,
-                                                        PostPortInitialized,
-                                                        PrePortDeleted,
-                                                        PostPortDeleted,
-                                                        ]}
-
-
-def register_custom_type(cls: t.Type[BaseType]) -> t.Type[BaseType]:
+def register_data_type(cls):
     RegisteredTypes.update({cls.__name__: cls})
-    logger.debug(f'new type has been registered: {cls.__name__}')
+    logger.debug(f'new data type has been registered: {cls.__name__}')
+
+    return cls
+
+
+def register_attribute(cls):
+    RegisteredAttributes.update({cls.__name__: cls})
+    logger.debug(f'new attribute has been registered: {cls.__name__}')
+
+    return cls
+
+
+def register_node(cls):
+    RegisteredNodes.update({cls.__name__: cls})
+    logger.debug(f'new node has been registered: {cls.__name__}')
+
+    return cls
+
+
+def register_port(cls):
+    RegisteredPorts.update({cls.__name__: cls})
+    logger.debug(f'new port has been registered: {cls.__name__}')
+
+    return cls
+
+
+def register_collection(cls):
+    RegisteredCollections.update({cls.__name__: cls})
+    logger.debug(f'new collection has been registered: {cls.__name__}')
+
+    return cls
+
+
+def register_event(cls):
+    RegisteredEvents.update({cls.__name__: cls})
+    logger.debug(f'new event has been registered: {cls.__name__}')
 
     return cls
