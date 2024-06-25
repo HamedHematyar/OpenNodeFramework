78f2c6c0801e5ad56c89d2c93687ac49890dfb68
hamed.hematyar <hamed.yar@outlook.com>
2024-06-24 17:11:06 -0700

    new register method

--- custom/data_types/data_types.py
+++ custom/data_types/data_types.py
@@ -1,8 +1,8 @@
 from backend.data_types import GenericStr
-from backend.registry import register_custom_type
+from backend.registry import register_data_type
 
 
-@register_custom_type
+@register_data_type
 class NodeName(GenericStr):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
