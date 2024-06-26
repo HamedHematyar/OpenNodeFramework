7ab8550d3c00f250c4e7aaa660375987fb825ffb
hamed.hematyar <hamed.yar@outlook.com>
2024-06-26 16:06:51 -0700

    cleanup meta

--- backend/meta.py
+++ backend/meta.py
@@ -1,12 +1,13 @@
 import uuid
+import typing as t
 
 from backend.logger import logger
 
 
 class SingletonMeta(type):
-    _instances = {}
+    _instances: t.Dict[type, t.Any] = {}
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs) -> t.Any:
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
@@ -17,94 +18,80 @@ class SingletonMeta(type):
 class EntityTrackerMeta(type):
     TYPE = None
 
-    def __init__(cls, name, bases, dct):
+    def __init__(cls, name, bases, dct) -> None:
         super().__init__(name, bases, dct)
 
-    def __call__(cls, *args, **kwargs):
-        # get existing id or generate a new one
+    def __call__(cls, *args, **kwargs) -> t.Any:
         unique_id = kwargs.get('id', str(uuid.uuid4()))
 
-        # if this is in app deserialization we need a new id
         if not InstanceManager().is_valid(unique_id):
             unique_id = str(uuid.uuid4())
 
         kwargs.update({'id': unique_id})
-
-        # initialize entity instance
         instance = super().__call__(*args, **kwargs)
-
-        # add instance to the manager
         InstanceManager().add_instance(instance)
-
         return instance
 
 
 class InstanceManager(metaclass=SingletonMeta):
-    def __init__(self):
-        self._instances = {}
+    def __init__(self) -> None:
+        self._instances: t.Dict[str, t.Any] = {}
 
-    def is_valid(self, unique_id):
-        return bool(not self._instances.get(unique_id))
+    def is_valid(self, unique_id: str) -> bool:
+        return unique_id not in self._instances
 
-    def add_instance(self, instance):
+    def add_instance(self, instance: t.Any) -> None:
         self._instances[instance.get_id()] = instance
 
-    def remove_instance(self, instance):
-        if instance.get_id() in self._instances:
-            self._instances.pop(instance.get_id())
+    def remove_instance(self, instance: t.Any) -> None:
+        instance_id = instance.get_id()
+        if instance_id in self._instances:
+            self._instances.pop(instance_id)
         else:
-            logger.warning(f'instance does not exist or already removed : {instance.get_id()}')
+            logger.warning(f'Instance does not exist or already removed: {instance_id}')
 
-    def get_instance(self, instance_id):
+    def get_instance(self, instance_id: str) -> t.Optional[t.Any]:
         return self._instances.get(instance_id)
 
-    def instances(self):
+    def instances(self) -> t.Dict[str, t.Any]:
         return self._instances
 
-    def clear_all(self):
+    def clear_all(self) -> None:
         self._instances.clear()
 
 
 class ReferenceManager(metaclass=SingletonMeta):
-    def __init__(self):
-        self._references = {}
+    def __init__(self) -> None:
+        self._references: t.Dict[str, list[t.Callable[[t.Any], None]]] = {}
 
-    def __enter__(self):
-
+    def __enter__(self) -> 'ReferenceManager':
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
         self._resolve_references()
         self._references.clear()
 
-    def request_instant_reference(self, instance_id):
+    def request_instant_reference(self, instance_id: str) -> t.Optional[t.Any]:
         return InstanceManager().get_instance(instance_id)
 
-    def request_deferred_reference(self, instance_id, reference_setter):
-        if not instance_id:
+    def request_deferred_reference(self, instance_id: str, reference_setter: t.Callable[[t.Any], None]) -> None:
+        if not instance_id or not reference_setter:
             return
-
-        if not reference_setter:
-            return
-
         self._request_reference(instance_id, reference_setter)
 
-    def _request_reference(self, instance_id, reference_setter):
-        logger.debug(f'requesting deferred reference : {instance_id} : {reference_setter}')
-
+    def _request_reference(self, instance_id: str, reference_setter: t.Callable[[t.Any], None]) -> None:
+        logger.debug(f'Requesting deferred reference: {instance_id} : {reference_setter}')
         if instance_id in self._references:
             self._references[instance_id].append(reference_setter)
         else:
-            self._references[instance_id] = [reference_setter, ]
+            self._references[instance_id] = [reference_setter]
 
-    def _resolve_references(self):
+    def _resolve_references(self) -> None:
         for instance_id, setters in self._references.items():
             instance = InstanceManager().get_instance(instance_id)
             if not instance:
-                logger.error(f'reference instance does not exist: {instance_id}')
+                logger.error(f'Reference instance does not exist: {instance_id}')
                 continue
-
             for setter in setters:
-                logger.debug(f'resolving reference : {instance} : {setter}')
+                logger.debug(f'Resolving reference: {instance} : {setter}')
                 setter(instance)
-
